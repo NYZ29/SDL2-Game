cc97c67ad83011624929d8f33aaea2c27a86cb9d
Nail Yusupov <u.nail2006@mail.ru>
2025-12-08 19:18:04 +0300

    Initial commit: SDL2 Game project Basic SDL2 Game game engine Window and renderer setup Texture and manager for image control Game loop with frame rate control Project structure fir Visual Studio

---
+++ SDL2 Game/Game.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <SDL.h>
+#include <SDL_image.h>
+#include <iostream>
+
+class Game
+{
+private:
+	int cnt = 0;
+	bool isRunning;
+	SDL_Window* window;
+	SDL_Renderer* renderer;
+public:
+	Game();
+	~Game();
+
+	void init(const char* title, int xpos, int ypos, int width, int height, bool fullscreen);
+	void handleEvents();
+	void update();
+	void render();
+	void clean();
+
+	bool running() { return isRunning; }
+};
